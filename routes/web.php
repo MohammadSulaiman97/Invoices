fa0a8ab2374ec1dc4c8440ec9d2474a6cddd23ed
asus <mohammadsulaiman870@gmail.com>
2021-01-12 14:07:40 +0200

    products

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InvoicesController;
 use App\Http\Controllers\SectionsController;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,5 +30,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('invoices', InvoicesController::class);
 
 Route::resource('sections', SectionsController::class);
+
+Route::resource('products', ProductsController::class);
 
 Route::get('/{page}', [AdminController::class, 'index']);
